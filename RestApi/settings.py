8587c2bb96482c8e9f71ed81168a80c82bec2e3e
Ishti97 <ishtiakrahman15@gmail.com>
2021-06-29 22:09:21 +0600

    User Profile Model Created

--- RestApi/settings.py
+++ RestApi/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+AUTH_USER_MODEL = 'profiles_api.Userprofile'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
